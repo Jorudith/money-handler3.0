b52c402c3b948af77bbd7b991b42653418ba0bc2
John Ryan C. Salvador <j0hnry4ns4lv4d0r@gmail.com>
2022-04-12 20:27:00 +0800

    Added app description in Main/app.py

--- Main/app.py
+++ Main/app.py
@@ -1,1 +1,13 @@
 from tkinter import *
+
+"""
+Money Handler App 3.0
+
+-- Author --
+John Ryan C. Salvador
+
+-- Changes --
++ Flexible/Responsive Interface
++ Bigger Log Preview
++ Added Time & Date to logging of transactions
+"""
