0db34c5e3f3b0d5a3cb49fc2389c8db3f26ae4ec
John Ryan C. Salvador <j0hnry4ns4lv4d0r@gmail.com>
2022-08-30 09:21:03 +0800

    Experimenting with frames

--- app.py
+++ app.py
@@ -12,10 +12,16 @@ with open("json/app_settings.json", "r") as file:
 
 root = Tk()
 root.title(AS.get("app_title"))
-root.iconbitmap()
+root.iconbitmap("img/favicon.ico")
 root.geometry(f"{AS.get('app_width')}x{AS.get('app_height')}")
 root.resizable(AS.get("app_isresizeable"), AS.get("app_isresizeable"))
 root.configure(bg=AS.get("app_bg"))
+
+left_frame = ttk.Frame(root, width=100, height=100)
+right_frame = ttk.Frame(root, width=100, height=100)
+
+left_frame.place(x=0, y=0)
+right_frame.place(x=101, y=0)
 
 
 root.mainloop()
