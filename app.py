2bb1693dfdacc7bbf78163430d1694e4951ea853
John Ryan C. Salvador <j0hnry4ns4lv4d0r@gmail.com>
2022-09-04 11:39:13 +0800

    Improved app.py and funcs.py at the second computer and brought it back to main computer

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # import gui module (tk)
-from tkinter import Tk
-from tkinter import ttk
+import tkinter as tk
 
 # import dependencies
 import json
@@ -10,21 +9,45 @@ import logger
 with open("json/app_settings.json", "r") as file:
     AS = dict(json.load(file))
 
-root = Tk()
+root = tk.Tk()
 root.title(AS.get("app_title"))
 root.iconbitmap("img/favicon.ico")
-root.geometry(f"{AS.get('app_width')}x{AS.get('app_height')}")
+root.geometry(f"{dict(AS.get('app_width')).get('default')}x{dict(AS.get('app_height')).get('default')}")
+root.minsize(width=dict(AS.get('app_width')).get('min'), height=dict(AS.get('app_height')).get('min'))
+root.maxsize(width=dict(AS.get('app_width')).get('max'), height=dict(AS.get('app_height')).get("max"))
 root.resizable(AS.get("app_isresizeable"), AS.get("app_isresizeable"))
 root.configure(bg=AS.get("app_bg"))
 
-left_frame = ttk.Frame(root, width=155, height=790)
-right_frame = ttk.Frame(root, width=830, height=790)
+# border initialization
+top_border = tk.Frame(root, background="#dddddd", width=10, height=10)
+center_border = tk.Frame(root, background="#dddddd", width=10, height=10)
+bot_border = tk.Frame(root, background="#dddddd", width=10, height=10)
+left_border = tk.Frame(root, background="#dddddd", width=10, height=10)
+right_border = tk.Frame(root, background="#dddddd", width=10, height=10)
 
-left_frame.place(x=5, y=5)
-right_frame.place(x=165, y=5)
+# main frame initialization
+side_frame = tk.Frame(root, background="gray", width=10, height=800)
+main_frame = tk.Frame(root, background="#14ee14", width=10, height=800)
 
-main_title = ttk.Label(left_frame, text="Money Handler", font=("Bahnschirft", 10))
+# border grid
+top_border.grid(row=0, column=0, columnspan=4)
+center_border.grid(row=0, column=2, sticky="ns")
+bot_border.grid(row=2, column=0, columnspan=4)
+left_border.grid(row=0, column=0, rowspan=2)
+right_border.grid(row=0, column=4, rowspan=2)
 
-main_title.place(x=0, y=0)
+# main frame grid
+side_frame.grid(row=1, column=1, sticky="nsew")
+main_frame.grid(row=1, column=3, sticky="nsew")
+
+# column weight configuration (for flexibility)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(3, weight=8)
+
+# row weight configuration (for flexibility)
+root.rowconfigure(1, weight=1)
+
+# side_frame child (button/label) initialization
+add_button = tk.Button(side_frame, text="ADD FUNDS")
 
 root.mainloop()
--- funcs.py
+++ funcs.py
@@ -1,11 +1,28 @@
+# import dependencies
+# import app
+
+# import tkinter for function purposes
+import tkinter as tk
+
+# import the main logger
 from logger import logger
 
-def log(function):
-    def wrapper():
+# app function logger
+def func_log(function):
+    def wrapper(amount):
         try:
-            function()
-        
+            function(amount)
+            logger.debug(f"{function} has been used")
         except:
-            logger.exception(f"{function} Something we don't know went wrong")
-
+            logger.warning(f"{function} Something we don't know went wrong")
     return wrapper
+
+@func_log
+def deduct_funds(amount):
+    pass
+
+@func_log
+def add_funds(amount):
+    pass
+
+deduct_funds(10)
